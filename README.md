847e268d5997124591c24a668cade6b75aead6df
TheComputerScientist <DRNNRW00M10C351S@studium.unict.it>
2025-05-03 15:28:35 +0200

    chore(main): Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# MSLesSeg 4 ICPR
+
+![Project-Header](./assets/imgs/01-Project-Header.jpeg)
+
+---
+
+## Overview
+
+This project is based on the recently concluded **ICPR challenge** organized by the **[IP Lab](https://iplab.dmi.unict.it/)** at the **University of Catania**, led by **Dr. F. Guarnera** and **Dr. A. Rondinella**. The challenge introduced a **carefully curated** and **refined dataset** of **brain MRI scans** from patients diagnosed with various forms of **Multiple Sclerosis**. All patients are from the city of **Catania**; however, the scans were acquired using **different MRI machines** across **multiple hospitals**, introducing valuable **inter-scanner variability**.
+
+The primary objectives of the challenge were twofold: to contribute a **novel**, **versatile dataset** to the **research community** and to **benchmark** the performance of **state-of-the-art (SOTA) models**. Participants were encouraged to explore **advanced strategies** such as **sophisticated preprocessing pipelines**, **variations in image registration spaces**, and **ensemble methods** involving **independently trained models**.
+
+You may find the **published paper [here](https://arxiv.org/abs/2410.07924)** and the **challenge’s homepage [here](https://iplab.dmi.unict.it/mfs/ms-les-seg/)**.
+
+---
+
+## Our Solution
+
+- Mention my solution here
+
+## Results
+
+- Talk about the obtained results
+
+## Setup & Replication
+
+- Describe in detail, from the acquisition of the dataset, to the installation of the requirements how to run the code locally
+
+---
+
---
+++ cloud_infrastructure/training_instance.tf
@@ -0,0 +1,88 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "instance-20250321-153644" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-20250321-153644"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20250311"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  guest_accelerator {
+    count = 1
+    type  = "projects/segformer4mslesseg/zones/us-central1-c/acceleratorTypes/nvidia-tesla-v100"
+  }
+
+  labels = {
+    goog-ec-src           = "vm_add-tf"
+    goog-ops-agent-policy = "v2-x86-template-1-4-0"
+  }
+
+  machine_type = "custom-8-43008"
+
+  metadata = {
+    enable-osconfig = "TRUE"
+  }
+
+  name = "instance-20250321-153644"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/segformer4mslesseg/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "TERMINATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "818223003091-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "us-central1-c"
+}
+
+module "ops_agent_policy" {
+  source          = "github.com/terraform-google-modules/terraform-google-cloud-operations/modules/ops-agent-policy"
+  project         = "segformer4mslesseg"
+  zone            = "us-central1-c"
+  assignment_id   = "goog-ops-agent-v2-x86-template-1-4-0-us-central1-c"
+  agents_rule = {
+    package_state = "installed"
+    version = "latest"
+  }
+  instance_filter = {
+    all = false
+    inclusion_labels = [{
+      labels = {
+        goog-ops-agent-policy = "v2-x86-template-1-4-0"
+      }
+    }]
+  }
+}
